e5e503eb902bce2b0c286aba299037b66558c633
vantuan88291 <vantuan88291@gmail.com>
2018-12-01 13:49:37 +0700

    first commit

---
+++ app/src/main/java/easy/asyntask/tuan88291/asynctaskeasy/MainActivity.java
@@ -0,0 +1,13 @@
+package easy.asyntask.tuan88291.asynctaskeasy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ library/src/main/java/easy/asyntask/tuan88291/library/Async.java
@@ -0,0 +1,4 @@
+package easy.asyntask.tuan88291.library;
+
+public class Async {
+}
