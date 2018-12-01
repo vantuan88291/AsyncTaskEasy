d3df9f65fdf486045bba5bd1d4f7ebc02ecc1b60
vantuan88291 <vantuan88291@gmail.com>
2018-12-01 14:01:31 +0700

    add builder

--- app/src/main/java/easy/asyntask/tuan88291/asynctaskeasy/MainActivity.java
+++ app/src/main/java/easy/asyntask/tuan88291/asynctaskeasy/MainActivity.java
@@ -3,11 +3,32 @@ package easy.asyntask.tuan88291.asynctaskeasy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import easy.asyntask.tuan88291.library.Async;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Async() {
+            @Override
+            protected String doBackground() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return "sleep 5s";
+            }
+
+            @Override
+            protected void onSuccess(String s) {
+                super.onSuccess(s);
+                //do your result
+            }
+        };
+
     }
 }
--- library/src/main/java/easy/asyntask/tuan88291/library/Async.java
+++ library/src/main/java/easy/asyntask/tuan88291/library/Async.java
@@ -1,4 +1,63 @@
 package easy.asyntask.tuan88291.library;
 
-public class Async {
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+
+@SuppressLint("StaticFieldLeak")
+public abstract class Async {
+    public Async() {
+
+        Asyn sync = new Asyn() {
+            @Override
+            protected void onLoading() {
+                Async.this.onLoading();
+            }
+            @Override
+            protected String doInBackGround() throws Exception{
+                return Async.this.doBackground();
+            }
+            @Override
+            protected void onSuccess(String s) {
+                Async.this.onSuccess(s);
+            }
+        };
+        sync.execute();
+    }
+
+
+    private abstract class Asyn extends AsyncTask<String, Integer, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+                return doInBackGround();
+            } catch (Exception e) {
+                return e.getLocalizedMessage();
+            }
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            onLoading();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            onSuccess(s);
+        }
+        protected abstract void onLoading();
+
+        protected abstract String doInBackGround() throws Exception;
+
+        protected abstract void onSuccess(String s);
+    }
+
+    protected abstract String doBackground() throws Exception;
+    protected void onSuccess(String s) {
+
+    }
+    protected void onLoading() {
+
+    }
 }
